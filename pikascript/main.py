12f0d250d45255fc215632289327b54ace56b190
lyon <liang6516@outlook.com>
2022-10-01 15:05:48 +0800

    fix main.py

--- pikascript/main.py
+++ pikascript/main.py
@@ -26,27 +26,3 @@ for i in range(9):
     label.set_text(str(i))
     label.center()
 
-a_row = lv.anim_t()
-a_row.init()
-a_row.set_var(cont)
-a_row.set_values(0, 10)
-a_row.set_repeat_count(lv.ANIM_REPEAT.INFINITE)
-
-a_row.set_time(500)
-a_row.set_playback_time(500)
-a_row.set_custom_exec_cb(lambda a,val: row_gap_anim(cont,val))
-lv.anim_t.start(a_row)
-
-a_col = lv.anim_t()
-a_col.init()
-a_col.set_var(cont)
-a_col.set_values(0, 10)
-a_col.set_repeat_count(lv.ANIM_REPEAT.INFINITE)
-
-a_col.set_time(3000)
-a_col.set_playback_time(3000)
-a_col.set_custom_exec_cb(lambda a,val: column_gap_anim(cont,val))
-
-lv.anim_t.start(a_col)
-
-
